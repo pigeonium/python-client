07d3ff24bdf165f2311b2119e284b27260a76062
h4ribote <github@h4ribote.net>
2025-07-09 17:31:15 +0900

    delVariable()の追加

--- funcHint.py
+++ funcHint.py
@@ -9,28 +9,28 @@ baseCurrency: Currency = Currency()
 CanselTransaction = CanselTransaction
 
 def hex2bytes(hex:str, length:int=None) -> bytes:
-    pass
+    ...
 
 def sha256(string:bytes) -> bytes:
-    pass
+    ...
 
 def sha3_256(string:bytes) -> bytes:
-    pass
+    ...
 
 def sha3_512(string:bytes) -> bytes:
-    pass
+    ...
 
 def getBalance(address:bytes, currencyId:bytes) -> int:
-    pass
+    ...
 
 def getCurrency(currencyId:bytes) -> Currency|None:
-    pass
+    ...
 
 def getSelfCurrency() -> Currency|None:
-    pass
+    ...
 
 def getTransaction(indexId:int) -> Transaction|None:
-    pass
+    ...
 
 def getTransactions(
         self,
@@ -48,31 +48,37 @@ def getTransactions(
         limit: int = 20,
         offset: int = 0
     ) -> list[Transaction]:
-    pass
+    ...
 
 def getVariable(address:bytes, varKey:bytes) -> bytes|None:
-    pass
+    ...
 
-def setVariable(varKey:bytes, varValue:bytes):
-    pass
+def setVariable(varKey:bytes, varValue:bytes|None):
+    if varValue is None:
+        delVariable(varKey)
+    else:
+        ...
+
+def delVariable(varKey:bytes):
+    ...
 
 # def transferFromUser(dest:bytes, currencyId:bytes, amount:int) -> Transaction:
-#     pass
+#     ...
 
 # def transferFromContract(dest:bytes, currencyId:bytes, amount:int) -> Transaction:
-#     pass
+#     ...
 
 def transfer(dest:bytes, currencyId:bytes, amount:int) -> Transaction:
-    pass
+    ...
 
 def burn(amount:int) -> Transaction:
-    pass
+    ...
 
 def mint(amount:int) -> Transaction:
-    pass
+    ...
 
 def createCurrency(name:str, symbol:str, supply:int) -> Transaction:
-    pass
+    ...
 
 def nextIndexId() -> int:
-    pass
+    ...
